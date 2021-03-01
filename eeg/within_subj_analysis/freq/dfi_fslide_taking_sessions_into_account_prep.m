65f58df38d26fee2881e8156830d5e0df5baece9
Steffen Buergers <sbuergers@gmail.com>
2021-03-01 15:24:58 -0500

    added license and made paths relative

--- eeg/within_subj_analysis/freq/dfi_fslide_taking_sessions_into_account_prep.m
+++ eeg/within_subj_analysis/freq/dfi_fslide_taking_sessions_into_account_prep.m
@@ -18,6 +18,24 @@
 % to first stimulus onset for each subject for channels
 % O1, Oz, O2, PO8, PO4, POz.
 % 
+% ===========================================================================
+%
+%     dfi (double flash illusion) codebase accompanying the manuscript ...
+%     Copyright (C) 2021  Steffen Buergers
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+%
 % ---
 % Steffen Buergers, sbuergers@gmail.com,
 % Last modified Feb. 2021
@@ -68,9 +86,7 @@ w          = [6 14];   % alpha search window
 
 
 for isubj = 1:N
-    
-    cd('D:\');
-    
+        
     subj_dir = fullfile(data_dir, subjvect{isubj}, task);
     
     % get participant, session and session/run information from user
@@ -305,7 +321,6 @@ for isubj = 1:N
         [~, eeg_fslide]  = dfi_eeg_analysis(s, eeg_zeropad);     
 
         % Save data
-        cd('D:\');
         mkdir(fullfile(subj_dir, an_fold, sess_fold));
         save(fullfile(subj_dir, an_fold, sess_fold, 'fslide_data_trls_all'), ...
             'eeg_fslide', 'beh_stim', '-v7.3');
