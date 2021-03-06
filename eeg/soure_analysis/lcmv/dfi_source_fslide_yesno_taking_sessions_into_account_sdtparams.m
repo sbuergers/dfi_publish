b839d0d0c973dfd661da270f76a54261e033d766
Steffen Buergers <sbuergers@gmail.com>
2021-03-06 14:48:24 -0500

    some small clean-up

--- eeg/soure_analysis/lcmv/dfi_source_fslide_yesno_taking_sessions_into_account_sdtparams.m
+++ eeg/soure_analysis/lcmv/dfi_source_fslide_yesno_taking_sessions_into_account_sdtparams.m
@@ -93,7 +93,7 @@ task     = 'yesno';
 channels_of_interest = {'src1', 'src2', 'src3', 'src4', 'src5', 'src6', 'src7'};
 
 
-% You can also directly get the data from D:\dfi_experiment_data\eeg_data\experiment
+% You can also directly get the data from g:\dfi_experiment_data\eeg_data\experiment
 btable = [];
 
 % loop over subjects
@@ -439,7 +439,7 @@ colavg = [0 0.6 0; 0 0 1; 1 0 0];
 title_vect = {'1F vs 2F', '1F1S vs 2F1S', '1F2S vs 2F2S'};
 dp_within_SE = nan(3,3);
 for icond = 1:3
-    % get Cousineau within subject SE for plottinD:
+    % get Cousineau within subject SE for plotting:
     % Cancel out between subject variability by subtracting the subject
     % mean from each subject and then adding the grand mean
     data = squeeze(dp_mat(:,icond,:));
@@ -470,7 +470,7 @@ colavg = [0 0.6 0; 0 0 1; 1 0 0];
 title_vect = {'1F vs 2F', '1F1S vs 2F1S', '1F2S vs 2F2S'};
 c_within_SE = nan(3,3);
 for icond = 1:3
-    % get Cousineau within subject SE for plottinD:
+    % get Cousineau within subject SE for plotting:
     % Cancel out between subject variability by subtracting the subject
     % mean from each subject and then adding the grand mean
     data = squeeze(c_mat(:,icond,:));
@@ -639,7 +639,7 @@ for isoa = 1:length(soas)
     tvect = time_windows(2:nt)-diff(time_windows);
     dp_within_SE = nan(size(squeeze(dp_mat_cont(:,:,:,1,:)),2),3,3);
     for icond = 1:3
-        % get Cousineau within subject SE for plottinD:
+        % get Cousineau within subject SE for plotting:
         % Cancel out between subject variability by subtracting the subject
         % mean from each subject and then adding the grand mean
         data = squeeze(dp_mat_cont(isoa,:,:,icond,:));
@@ -685,7 +685,7 @@ for isoa = 1:length(soas)
     tvect = time_windows(2:nt)-diff(time_windows);
     c_within_SE = nan(size(squeeze(c_mat_cont(:,:,:,1,:)),2),3,3);
     for icond = 1:3
-        % get Cousineau within subject SE for plottinD:
+        % get Cousineau within subject SE for plotting:
         % Cancel out between subject variability by subtracting the subject
         % mean from each subject and then adding the grand mean
         data = squeeze(c_mat_cont(isoa,:,:,icond,:));
@@ -729,5 +729,5 @@ save(fullfile(save_dir, 'sd_params_d_c_yesno.mat'), ...
 warning('on','all')
 
 
-% // eof
+% eof
 
