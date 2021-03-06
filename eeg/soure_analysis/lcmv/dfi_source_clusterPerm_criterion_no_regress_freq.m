46c898ecd00c3f83d9541169540f50e8443e425a
Steffen Buergers <sbuergers@gmail.com>
2021-03-06 17:17:00 -0500

    linting changes

--- eeg/soure_analysis/lcmv/dfi_source_clusterPerm_criterion_no_regress_freq.m
+++ eeg/soure_analysis/lcmv/dfi_source_clusterPerm_criterion_no_regress_freq.m
@@ -150,7 +150,7 @@ for icond = 1:3
 
     % show specs
     fprintf(['\n', '\n',  ...
-             'Running cluster permutation witD:', '\n', ...
+             'Running cluster permutation with:', '\n', ...
             ['Method        = ', method], '\n', ...
             ['Trials        = ', trltype], '\n', ...
             ['Channels      = ', chan], '\n', ...
@@ -277,20 +277,5 @@ for icond = 1:3
 end
 
 
-
-
 % eof
-
-
-
-
-
-
-
-
-
-
-
-
-
 
