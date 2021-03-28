5da44e86af96406d20884932e7a353d9e9661c02
Steffen Buergers <sbuergers@gmail.com>
2021-03-28 15:03:22 -0400

    added script desc

--- eeg/within_subj_analysis/freq/dfi_check_fslide_effect_consistency_yn_vs_ynt.m
+++ eeg/within_subj_analysis/freq/dfi_check_fslide_effect_consistency_yn_vs_ynt.m
@@ -1,6 +1,32 @@
 % Check if participants have consistently higher or lower sensitivity or
 % bias depending on alpha frequency in both experiments (yesno and
 % yes-nothreshold). 
+%
+% Parent script(s): 
+%   dfi_fslide_yesno_taking_sessions_into_account_sdtparams.m
+%   dfi_fslide_taking_sessions_into_account_sdtparams.m
+% 
+% ===========================================================================
+%
+%     dfi (double flash illusion) codebase accompanying the manuscript ...
+%     Copyright (C) 2021  Steffen Buergers
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+%
+% ---
+% Steffen Buergers, sbuergers@gmail.com,
+% Last modified Mar. 2021
 
 
 clc
