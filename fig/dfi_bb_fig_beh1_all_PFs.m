047dad90ff6670b2bd0c101f3dae54f3826009d8
Steffen Buergers <sbuergers@gmail.com>
2021-03-30 17:35:05 -0400

    removed poor pffits also from per corr in pf figure

--- fig/dfi_bb_fig_beh1_all_PFs.m
+++ fig/dfi_bb_fig_beh1_all_PFs.m
@@ -61,25 +61,24 @@ load(fullfile(load_dir, folder, 'dataPF_joined_and_sep_fits_combined_pffit.mat')
 pffit_yn_pooled = pffit; clear pffit
 
 % Accumulate parameters of subjects in matrices (2 event conditions)
-[threshold_matrix, slope_matrix, lapse_matrix] = deal(nan(size(pffit_yn_pooled,2), 3));
-[pc_mat] = deal(nan(size(pffit_yn_pooled,2), 3, 8));
+[threshold_matrix, slope_matrix, lapse_matrix] = deal(nan(size(pffit_yn_pooled,1), 3));
+[pc_mat] = nan(size(pffit_yn_pooled,1), 3, 8);
 for isubj = 1:length(pffit_yn_pooled)
     for icond = 1:length(pffit_yn_pooled{isubj})
-        threshold_matrix(isubj, icond) = pffit_yn_pooled{isubj}{icond}.par(1);
-        slope_matrix(isubj, icond) = pffit_yn_pooled{isubj}{icond}.par(2);
-        lapse_matrix(isubj, icond) = pffit_yn_pooled{isubj}{icond}.par(4);
-        pc_mat(isubj, icond, :) = pffit_yn_pooled{isubj}{icond}.perCor;
+        if ~reject(isubj, icond)
+            threshold_matrix(isubj, icond) = pffit_yn_pooled{isubj}{icond}.par(1);
+            slope_matrix(isubj, icond) = pffit_yn_pooled{isubj}{icond}.par(2);
+            lapse_matrix(isubj, icond) = pffit_yn_pooled{isubj}{icond}.par(4);
+            pc_mat(isubj, icond, :) = pffit_yn_pooled{isubj}{icond}.perCor;
+        end
     end % condition
 end % subject
 
-% remove bad fits
-threshold_matrix(reject) = nan;
-slope_matrix(reject) = nan;
-lapse_matrix(reject) = nan;
+% remember bad fits
 reject_ynpool = reject;
 
-pc_GA_yn_pool = squeeze(mean(pc_mat,1));
-pc_SE_yn_pool = squeeze(std(pc_mat)) ./ sqrt(size(pc_mat,1));
+pc_GA_yn_pool = squeeze(nanmean(pc_mat,1));
+pc_SE_yn_pool = squeeze(nanstd(pc_mat)) ./ sqrt(size(pc_mat,1));
 
 
 
@@ -92,26 +91,25 @@ load(fullfile(load_dir, folder, 'dataPF_joined_and_sep_fits_combined_pffit.mat')
 pffit_2ifc = pffit; clear pffit
 
 % Accumulate parameters of subjects in matrices
-[threshold_matrix, slope_matrix, lapse_matrix] = deal(nan(size(pffit_2ifc,2), 3));
-[pc_mat, numcor_mat, outof_mat] = deal(nan(size(pffit_2ifc,2), 3, 8));
+[threshold_matrix, slope_matrix, lapse_matrix] = deal(nan(size(pffit_2ifc,1), 3));
+[pc_mat, numcor_mat, outof_mat] = deal(nan(size(pffit_2ifc,1), 3, 8));
 for isubj = 1:length(pffit_2ifc)
     for icond = 1:length(pffit_2ifc{isubj})
-        threshold_matrix(isubj, icond) = pffit_2ifc{isubj}{icond}.par(1);
-        slope_matrix(isubj, icond)  = pffit_2ifc{isubj}{icond}.par(2);
-        lapse_matrix(isubj, icond)  = pffit_2ifc{isubj}{icond}.par(4);
-        if isubj == 1
-            idvect = [1,2,3,4,5,6,8];
-        else
-            idvect = 1:8;
+        if ~reject(isubj,icond)
+            threshold_matrix(isubj, icond) = pffit_2ifc{isubj}{icond}.par(1);
+            slope_matrix(isubj, icond)  = pffit_2ifc{isubj}{icond}.par(2);
+            lapse_matrix(isubj, icond)  = pffit_2ifc{isubj}{icond}.par(4);
+            if isubj == 1
+                idvect = [1,2,3,4,5,6,8];
+            else
+                idvect = 1:8;
+            end
+            pc_mat(isubj, icond, idvect)     = pffit_2ifc{isubj}{icond}.perCor;
         end
-        pc_mat(isubj, icond, idvect)     = pffit_2ifc{isubj}{icond}.perCor;
     end % condition
 end % subject
 
-% remove bad fits
-threshold_matrix(reject) = nan;
-slope_matrix(reject) = nan;
-lapse_matrix(reject) = nan;
+% remember bad fits
 reject_2ifc = reject;
 
 pc_GA_2ifc       = squeeze(nanmean(pc_mat,1));
@@ -143,16 +141,50 @@ dtsz = 5;  % size of PF dots
 
 
 
-%% Figure YN pooled and 2IFC psychometric functions
+%% Figure 2IFC and YN pooled psychometric functions
 
 fh3 = figure('color', [1 1 1], 'Position', [0, 0, 350*1.5, 350]);
 ha = tight_subplot(2, 3,[0.02 0.014],[0.02],[0.02]);
 
-
-% Plot YN pooled PFs
+% Plot 2IFC psychometric functions
 nj = 20;
 ni = 3; 
 id = 1:3;
+Ns = nan(20,3);
+for i = 1:ni % condition
+    axes(ha(id(i)));
+    
+    for j = 1:nj % subj
+        if ~reject_2ifc(j,i)
+            soa  = pffit_2ifc{j}{i}.soa;
+            soal = round((soa*1000))/1000; % round for plotting to two decimal places
+
+            % Plot PFs (individual subject)
+            soaHR = min(soa):max(soa)/1000:max(soa);
+            pffit_2ifcML = PF(pffit_2ifc{j}{i}.par,    soaHR);
+            plot(soaHR, pffit_2ifcML, '-', 'Color', col.indiv{i}, 'LineWidth', lw); hold on
+            Ns(j,i) = 1;
+        end
+    end % end j
+    % Plot data
+    errorbar(StimLevels(i,:), pc_GA_2ifc(i,:), pc_SE_2ifc(i,:), pc_SE_2ifc(i,:), 'Color', 'k', 'LineWidth', 1.5); hold on
+    plot(StimLevels(i,:), pc_GA_2ifc(i,:), '-', 'Color', col.points{i}, 'linewidth', 1.5); hold on
+    plot(StimLevels(i,:), pc_GA_2ifc(i,:), 'ko', 'MarkerSize', dtsz, 'MarkerFaceColor', col.points{i}); hold on
+    
+    % plot settings
+    set(gca, 'Xtick', soa); yl = [0.5-0.1, 1.05]; ylim(yl);
+    set(gca, 'Xlim', [0 max(soa)+min(soa)])
+    set(gca, 'YTickLabel', []);
+    set(gca, 'XTickLabel', []);
+    set(gca, 'YTick', 0.5:0.25:1.5);
+    xlim([min(soa)-0.025, max(soa)+0.025]);
+    set(gca,'TickDir','out')
+    set(gca,'TickLength',[0.02, 0.02])
+end % i
+Ns
+
+% Plot YN pooled PFs
+id = 4:6;
 Ns = nan(20,3);
 for i = 1:ni % condition
     axes(ha(id(i)));
@@ -186,41 +218,6 @@ for i = 1:ni % condition
 end % i
 Ns
 
-
-% Plot 2IFC psychometric functions
-id = 4:6;
-Ns = nan(20,3);
-for i = 1:ni % condition
-    axes(ha(id(i)));
-    
-    for j = 1:nj % subj
-        if ~reject_2ifc(j,i)
-            soa  = pffit_2ifc{j}{i}.soa;
-            soal = round((soa*1000))/1000; % round for plotting to two decimal places
-
-            % Plot PFs (individual subject)
-            soaHR = min(soa):max(soa)/1000:max(soa);
-            pffit_2ifcML = PF(pffit_2ifc{j}{i}.par,    soaHR);
-            plot(soaHR, pffit_2ifcML, '-', 'Color', col.indiv{i}, 'LineWidth', lw); hold on
-            Ns(j,i) = 1;
-        end
-    end % end j
-    % Plot data
-    errorbar(StimLevels(i,:), pc_GA_2ifc(i,:), pc_SE_2ifc(i,:), pc_SE_2ifc(i,:), 'Color', 'k', 'LineWidth', 1.5); hold on
-    plot(StimLevels(i,:), pc_GA_2ifc(i,:), '-', 'Color', col.points{i}, 'linewidth', 1.5); hold on
-    plot(StimLevels(i,:), pc_GA_2ifc(i,:), 'ko', 'MarkerSize', dtsz, 'MarkerFaceColor', col.points{i}); hold on
-    
-    % plot settings
-    set(gca, 'Xtick', soa); yl = [0.5-0.1, 1.05]; ylim(yl);
-    set(gca, 'Xlim', [0 max(soa)+min(soa)])
-    set(gca, 'YTickLabel', []);
-    set(gca, 'XTickLabel', []);
-    set(gca, 'YTick', 0.5:0.25:1.5);
-    xlim([min(soa)-0.025, max(soa)+0.025]);
-    set(gca,'TickDir','out')
-    set(gca,'TickLength',[0.02, 0.02])
-end % i
-Ns
 
 fh3.Renderer = 'painters'; 
 mkdir(save_dir);
