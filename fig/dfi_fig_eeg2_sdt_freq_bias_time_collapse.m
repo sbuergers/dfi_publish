80d608f92786685011e74314c2647062b25bed04
sbuergers <sbuergers@gmail.com>
2021-04-25 17:21:53 -0400

    added dprime time collapsed figures for LCMV analysis

--- fig/dfi_fig_eeg2_sdt_freq_bias_time_collapse.m
+++ fig/dfi_fig_eeg2_sdt_freq_bias_time_collapse.m
@@ -35,6 +35,16 @@ clc
 close all
 clear all
 
+% experiment script folder
+try
+    addpath(genpath('dfi'))
+catch
+    warning('Cannot find dfi folder')
+end
+
+% run startup function
+dfi_startup
+
 % paths
 data_dir = fullfile('dfi_experiment_data', 'eeg_data', 'experiment');
 main_dir = fullfile(data_dir, 'sdt', 'freq_slide');
@@ -61,7 +71,7 @@ fh1 = figure('color', [1 1 1], 'Position', [0, 0, 427, 705]);
 ha = tight_subplot(6, 4,[0.02 0.02],[0.02],[0.02]);
 
 
-%% Row 1 (yn_intermsoas, sensor)
+%% Column 1 (yn_intermsoas, sensor)
 clear fslide_GA tif sem_f1 sem_f2 stat
 
 load(fullfile(main_dir, 'sd_params_d_c_yesno_tcollapse.mat'));
@@ -96,12 +106,82 @@ for icond = 1:3
 end % condition loop
 
 
-%% Row 2 (ynt sensor)
+%% Column 2 (ynt sensor)
 clear fslide_GA tif sem_f1 sem_f2 stat
 
 load(fullfile(main_dir, 'sd_params_d_c_tcollapse.mat'));
 
 axes(ha(2));
+
+xl = [0.5 3.5];
+yl = [0.9, 2.4];
+
+for icond = 1:3
+    
+    errorbar((icond-1)+0.9, mean(dp_mat(:,icond,1)), dp_within_SE(icond,1), dp_within_SE(icond,1), ...
+        'Color', col1{icond}, 'LineWidth', 1.5); hold on
+    errorbar((icond-1)+1.1, mean(dp_mat(:,icond,3)), dp_within_SE(icond,3), dp_within_SE(icond,3), ...
+        'Color', col2{icond}, 'LineWidth', 1.5);
+    plot((icond-1)+0.9, mean(dp_mat(:,icond,1)), ...
+        'ko', 'MarkerSize', 3, 'MarkerFaceColor', col1{icond});
+    plot((icond-1)+1.1, mean(dp_mat(:,icond,3)), ...
+        'ko', 'MarkerSize', 3, 'MarkerFaceColor', col2{icond});
+    
+    xlim(xl)
+    ylim(yl)
+    yticks = 0:0.5:100;  set(gca, 'YTick', yticks);
+    set(gca, 'xticklabel', [])
+    set(gca, 'yticklabel', [])
+    
+    box off
+    set(gca,'TickDir','out')
+    set(gca,'TickLength',[0.02, 0.02])
+    set(gca,'XColor','k','YColor','k')
+    
+end % condition loop
+
+
+%% Column 3 (yn_intermsoas, LCMV)
+clear fslide_GA tif sem_f1 sem_f2 stat
+
+load(fullfile(src_dir, 'sd_params_d_c_yesno_tcollapse.mat'));
+
+axes(ha(3));
+
+xl = [0.5 3.5];
+yl = [0.9, 2.4];
+
+for icond = 1:3
+
+    errorbar((icond-1)+0.9, mean(dp_mat(:,icond,1)), dp_within_SE(icond,1), dp_within_SE(icond,1), ...
+        'Color', col1{icond}, 'LineWidth', 1.5); hold on
+    errorbar((icond-1)+1.1, mean(dp_mat(:,icond,3)), dp_within_SE(icond,3), dp_within_SE(icond,3), ...
+        'Color', col2{icond}, 'LineWidth', 1.5);
+    plot((icond-1)+0.9, mean(dp_mat(:,icond,1)), ...
+        'ko', 'MarkerSize', 3, 'MarkerFaceColor', col1{icond});
+    plot((icond-1)+1.1, mean(dp_mat(:,icond,3)), ...
+        'ko', 'MarkerSize', 3, 'MarkerFaceColor', col2{icond});
+    
+    xlim(xl)
+    ylim(yl)
+    yticks = 0:0.5:100;  set(gca, 'YTick', yticks);
+    set(gca, 'xticklabel', [])
+    set(gca, 'yticklabel', [])
+    
+    box off
+    set(gca,'TickDir','out')
+    set(gca,'TickLength',[0.02, 0.02])
+    set(gca,'XColor','k','YColor','k')
+    
+end % condition loop
+
+
+%% Column 4 (ynt LCMV)
+clear fslide_GA tif sem_f1 sem_f2 stat
+
+load(fullfile(src_dir, 'sd_params_d_c_tcollapse.mat'));
+
+axes(ha(4));
 
 xl = [0.5 3.5];
 yl = [0.9, 2.4];
@@ -141,7 +221,7 @@ fh2 = figure('color', [1 1 1], 'Position', [0, 0, 427, 400]);
 ha = tight_subplot(6, 4,[0.02 0.02],[0.02],[0.02]);
 
 
-%% Row 1 (yn_intermsoas, sensor)
+%% Column 1 (yn_intermsoas, sensor)
 
 load(fullfile(main_dir, 'sd_params_d_c_yesno_tcollapse.mat'));
 
@@ -171,12 +251,74 @@ set(gca,'TickDir','out')
 set(gca,'TickLength',[0.02, 0.02])
 set(gca,'XColor','k','YColor','k')
 
-    
-%% Row 4 (ynt sensor)
+
+%% Column 2 (ynt sensor)
 
 load(fullfile(main_dir, 'sd_params_d_c_tcollapse.mat'));
 
 axes(ha(2));
+
+xl = [0.5 3.5];
+yl = [-1, 1];
+
+bfs = nan(3,1);
+for icond = 1:3
+    
+    [~, ~, ~, stats] = ttest(dp_mat(:,icond,1) - dp_mat(:,icond,3));
+    t = stats.tstat;
+    df = stats.df;
+    bfs(icond) = t1smpbf(t, df+1);
+end
+
+bar(log10(bfs)); hold on 
+
+xlim(xl)
+ylim(yl)
+set(gca, 'xticklabel', [])
+set(gca, 'yticklabel', [])
+
+box off
+set(gca,'TickDir','out')
+set(gca,'TickLength',[0.02, 0.02])
+set(gca,'XColor','k','YColor','k')
+
+
+%% Column 3 (yn_intermsoas, LCMV)
+
+load(fullfile(src_dir, 'sd_params_d_c_yesno_tcollapse.mat'));
+
+axes(ha(3));
+
+xl = [0.5 3.5];
+yl = [-1, 1];
+
+bfs = nan(3,1);
+for icond = 1:3
+    
+    [~, ~, ~, stats] = ttest(dp_mat(:,icond,1) - dp_mat(:,icond,3));
+    t = stats.tstat;
+    df = stats.df;
+    bfs(icond) = t1smpbf(t, df+1);
+end
+
+bar(log10(bfs)); hold on 
+
+xlim(xl)
+ylim(yl)
+set(gca, 'xticklabel', [])
+set(gca, 'yticklabel', [])
+
+box off
+set(gca,'TickDir','out')
+set(gca,'TickLength',[0.02, 0.02])
+set(gca,'XColor','k','YColor','k')
+
+
+%% Column 4 (ynt LCMV)
+
+load(fullfile(src_dir, 'sd_params_d_c_tcollapse.mat'));
+
+axes(ha(4));
 
 xl = [0.5 3.5];
 yl = [-1, 1];
