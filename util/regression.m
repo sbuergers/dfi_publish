a2aa60cd7443fd65c7dffa764c405cc2c51c773f
sbuergers <sbuergers@gmail.com>
2021-04-26 21:19:09 -0400

    added regression helper function if neural network toolbox unavailable

---
+++ util/regression.m
@@ -0,0 +1,13 @@
+function [r, b1, b0] = regression(x, y, varargin)
+% Takes column vectors x and y as input and returns
+% Pearson's correlation coefficient, slope and intercept
+% parameters of a simple OLS regression.
+%
+% NOTE: This function is used as a proxy for the 'regression'
+% function of the Neural Network Toolbox and therefore takes
+% additional arguments that are unnecessary here.
+    p = polyfit(x, y, 1);
+    b0 = p(2);  
+    b1 = p(1);
+    r = corr(x, y, 'type', 'Pearson', 'rows', 'complete');
+end
