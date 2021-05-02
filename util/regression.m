b097c9681e26af1a10922d304bd0c23d9ffcbc8f
sbuergers <sbuergers@gmail.com>
2021-05-02 12:16:36 -0400

    allow handling of nans

--- util/regression.m
+++ util/regression.m
@@ -6,8 +6,16 @@ function [r, b1, b0] = regression(x, y, varargin)
 % NOTE: This function is used as a proxy for the 'regression'
 % function of the Neural Network Toolbox and therefore takes
 % additional arguments that are unnecessary here.
+    
+    % remove NaNs
+    keep = isfinite(y) & isfinite(x);
+    x = x(keep);
+    y = y(keep);
+    
+    % compute betas and pearson correlation
     p = polyfit(x, y, 1);
     b0 = p(2);  
     b1 = p(1);
     r = corr(x, y, 'type', 'Pearson', 'rows', 'complete');
+    
 end
